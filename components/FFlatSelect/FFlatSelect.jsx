0402314479580766ec9871faf76ba04c490efd08
liusong1111 <liusong1111@gmail.com>
2016-03-31 17:14:38 +0800

    FFlatSelect dropdown弹出/收起

--- components/FFlatSelect/FFlatSelect.jsx
+++ components/FFlatSelect/FFlatSelect.jsx
@@ -10,29 +10,45 @@ export default class FFlatSelect extends React.Component {
     }
   }
 
-  handleInputFocus() {
-    this.setState({isOpen: true})
+  handleOnClickPanel(event) {
+    let willOpen = !this.state.isOpen
+    this.setState({isOpen: willOpen}, () => {
+      if (willOpen) {
+        this.refs.dropdown.focus()
+      }
+    })
   }
 
-  handleInputBlur() {
-    //this.setState({isOpen: false})
-    console.log(document.activeElement)
+  handleDropdownBlur(event) {
+    if (event.relatedTarget && event.relatedTarget.isSameNode(this.refs.input)) {
+      return
+    }
+    this.setState({isOpen: false})
+  }
+
+  handleOnSelectItem(event, option) {
+    event.preventDefault()
+    event.stopPropagation()
+    this.props.onChange(option.value)
+    this.setState({isOpen: false})
   }
 
   render() {
     let dropdownItems = this.props.options.map(option => <li key={option.value}
-                                                             onClick={() => this.props.onChange(option.value)}
+                                                             onClick={(event) => this.handleOnSelectItem(event, option)}
                                                              className={styles.dropdownItem}>{option.label}</li>)
-    return (<div className={styles.flatSelect}>
+    return (<div className={styles.flatSelect} onClick={this.handleOnClickPanel.bind(this)}>
       <input type="text"
+             ref="input"
              readOnly="readonly"
              className={styles.inputSmall}
              value={this.props.value}
              placeholder={this.props.placeholder}
-             onFocus={this.handleInputFocus.bind(this)}
-             onBlur={this.handleInputBlur.bind(this)}
       />
-      <ul ref="dropdown" className={classNames(styles.dropdown, {hidden: !this.state.isOpen})}>
+      <ul ref="dropdown"
+          className={classNames(styles.dropdown, {hidden: !this.state.isOpen})}
+          tabIndex="0"
+          onBlur={this.handleDropdownBlur.bind(this)}>
         {dropdownItems}
       </ul>
     </div>)
